4a16c337d75cbda3a842241e0b78be041f71a137
Taras Omelianenko <t.omelianenko@apibest.com>
2024-02-02 20:07:33 +0200

    Remove build artifacts

--- build/upesy_wroom/FrameworkArduino/esp32-hal-tinyusb.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/upesy_wroom/FrameworkArduino/esp32-hal-tinyusb.c.o: \
- /Users/miroling/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-tinyusb.c \
- /Users/miroling/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
